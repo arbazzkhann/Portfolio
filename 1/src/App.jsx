f968256c6e8f14c285df0dc6ead5e16c7b62484b
Arbaz Khan <arbazkha78az@gmail.com>
2025-07-06 13:18:03 +0530

    cretaing routes

--- 1/src/App.jsx
+++ 1/src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
 
+//import form react-router-dom
+import { Routes, Route } from 'react-router-dom';
+
+//import pages
+import Home from '../src/Containers/Home/index'
+import About from '../src/Containers/About/index'
+import Contact from '../src/Containers/Contact/index'
+import Portfolio from '../src/Containers/Portfolio/index'
+import Resume from '../src/Containers/Resume/index'
+import Skills from '../src/Containers/Skills/index'
+
 
 function App() {
 
   return (
     <>
       
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/portfolio' element={<Portfolio />}/>
+        <Route path='/resume' element={<Resume />}/>
+        <Route path='/skills' element={<Skills />}/>
+      </Routes>
+
     </>
   )
 }
